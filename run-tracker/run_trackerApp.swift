8e9bdd44de7e2f27c62d4b1d03287350f8024c5b
Kayz-mann <kayodefredrickbalogun@gmail.com>
2025-03-18 11:22:36 +0100

    Initial Commit

---
+++ run-tracker/run_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  run_trackerApp.swift
+//  run-tracker
+//
+//  Created by Balogun Kayode on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct run_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
