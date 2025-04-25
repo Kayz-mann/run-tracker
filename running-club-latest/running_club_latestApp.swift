616ab16ceb573ba282f1852669c5a1e05165c6a5
Kayz-mann <kayodefredrickbalogun@gmail.com>
2025-04-25 01:45:20 +0100

    Initial Commit

---
+++ running-club-latest/running_club_latestApp.swift
@@ -0,0 +1,17 @@
+//
+//  running_club_latestApp.swift
+//  running-club-latest
+//
+//  Created by Balogun Kayode on 25/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct running_club_latestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ running-club-latestTests/running_club_latestTests.swift
@@ -0,0 +1,17 @@
+//
+//  running_club_latestTests.swift
+//  running-club-latestTests
+//
+//  Created by Balogun Kayode on 25/04/2025.
+//
+
+import Testing
+@testable import running_club_latest
+
+struct running_club_latestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
